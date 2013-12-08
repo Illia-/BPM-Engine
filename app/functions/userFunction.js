297e2b8fa84c0858de8e974adfce511e5cec54e5
Andrey <fandm.mail@gmail.com>
2013-12-08 21:24:33 +0200

    Update userFunction.js
    Добавил в testFunc deferred.reject(), чтобы избежать зависаний при несуществующих карточках документов.

--- app/functions/userFunction.js
+++ app/functions/userFunction.js
@@ -8,12 +8,16 @@ define([],
     function testFunc(engine, db, workflow) {
     	var deferred = Q.defer();
     	var cardId = workflow.cardId;
-    	db.getDoc(cardId).then(function(doc) {
-    		var result = doc.result;
-    		engine.setVariable(workflow._id, 'result', result).then(function(res) {
-    			deferred.resolve(res);
-    		});
-    	});
+    	db.getDoc(cardId)
+      	.then(function(doc) {
+      		var result = doc.result;
+      		engine.setVariable(workflow._id, 'result', result).then(function(res) {
+      			deferred.resolve(res);
+      		});
+      	})
+      	.fail(function() {
+      		deferred.reject();
+      	});
     	return deferred.promise;
     }
   });
