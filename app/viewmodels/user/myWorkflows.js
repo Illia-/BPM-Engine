435dda114bea31904c40f47ca11df638b0e49fb3
Andrey <fandm.mail@gmail.com>
2013-12-11 13:40:49 +0200

    Update myWorkflows.js

--- app/viewmodels/user/myWorkflows.js
+++ app/viewmodels/user/myWorkflows.js
@@ -8,6 +8,12 @@ define(['services/bpmEngine', 'services/appSecurity'],
 
     function activate(){
       engine.getWorkflowsByUser(appSecurity.user().name).then(function(data){
+        for (var i = 0; i < data.length; i++) {
+          var dtc = new Date(data[i].value.createDate);
+          var dtu = new Date(data[i].value.updateDate);
+          data[i].value.createDate = dtc.getDate()+'.'+dtc.getMonth()+'.'+dtc.getFullYear()+' '+dtc.getHours()+':'+dtc.getMinutes()+':'+dtc.getSeconds();
+          data[i].value.updateDate = dtu.getDate()+'.'+dtu.getMonth()+'.'+dtu.getFullYear()+' '+dtu.getHours()+':'+dtu.getMinutes()+':'+dtu.getSeconds();
+        }
         viewModel.workflows(data)
       });
     }
