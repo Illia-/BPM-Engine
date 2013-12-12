710d9760106779f11151f00e3220e183bf0859c2
illia.okhrimenko <illia.okhrimenko@customertimes.com>
2013-12-12 12:20:27 +0200

    доавлена возможность фильтравать в Мойх сценариях и сценарии отсортированы по дате создания в порядке убывания

--- app/viewmodels/user/myWorkflows.js
+++ app/viewmodels/user/myWorkflows.js
@@ -1,17 +1,37 @@
 define(['services/bpmEngine', 'services/appSecurity', 'helpers/date'],
   function(engine, appSecurity, date) {
     var viewModel = {
-      activate:activate,
-      workflows:ko.observableArray([])
+      activate    : activate,
+      workflows   : ko.observableArray([]),
+      statusFilter: ko.observable(null)
     }
+
+    viewModel.filteredWorkflows = ko.computed(function() {
+      var result = ko.utils.arrayFilter(viewModel.workflows(), function(workflow) {
+        console.log(viewModel.statusFilter())
+        if(viewModel.statusFilter() != 'all') {
+          return workflow.value.status == viewModel.statusFilter();
+        }
+        return true;
+      });
+
+      result.sort(function(a, b){
+        if(a.value.createDate > b.value.createDate){
+          return -1;
+        }
+        return 1;
+      });
+
+      return result;
+    });
+
     return viewModel;
 
-    function activate(){
-      engine.getWorkflowsByUser(appSecurity.user().name).then(function(data){
-        for (var i = 0; i < data.length; i++) {
-          var createDate = data[i].value.createDate;
-          data[i].value.createDate = date.formatDate(createDate);
-          data[i].value.createTime = date.formatTime(createDate);
+    function activate() {
+      engine.getWorkflowsByUser(appSecurity.user().name).then(function(data) {
+        for(var i = 0; i < data.length; i++) {
+          data[i].value.createDateFormatted = date.formatDate(data[i].value.createDate);
+          data[i].value.createTime = date.formatTime(data[i].value.createDate);
           data[i].value.updateDate = date.formatDateTime(data[i].value.updateDate);
         }
         viewModel.workflows(data)
